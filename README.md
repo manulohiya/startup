3d66b8616e2e925f6541211d2ba6335d168c3890
Manu Lohiya <manulohiya@gmail.com>
2015-07-23 16:05:58 -0700

    Updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# startup
+# Idea Hunt
+===========
+
+Post your startup idea on Idea Hunt and get suggestions from the community on how to make it better. Its like Product Hunt...but for Ideas. 
+
+##Overview
+
+Idea Hunt allows budding entrepreneurs to post their startup idea, share the idea's unique URL with their friends and family and enable them to provide helpful feedback before the entrepreneur spends too much time in building the idea
+
+##Motivation
+
+The motivation for this project is to empower budding enrepreneurs to be able to collect helpful feedback from the community for free!
+
+
+##Built With:
+
+* Node.js
+* Express
+* MongoDB
+* jQuery/AJAX
+* Bootstrap
+
+##Installs/Dependencies:
+
+In order to run locally, this app requires the following steps:
+* Clone Repo
+* Install the necessary node modules (see below for full list)
+* Start server
+
+In order to run locally, this app requires the following node modules
+* body-parser
+* cors
+* express
+* express-session
+* mongoose
+* underscore
+
+
+
+
+###Find it here: [http://startupwdi.herokuapp.com/](http://startupwdi.herokuapp.com/)
+
+
+
+--------------------
+
+![ScreenShot](public/images/Screenshot 2015-07-23 16.00.01.png)
---
+++ test/tests.js
@@ -0,0 +1,52 @@
+//
+// tests.js
+//
+
+var request = require('request');
+var expect = require('chai').expect;
+var cheerio = require("cheerio");
+var chai = require("chai");
+var mocha = require("mocha");
+var baseUrl = 'http://localhost:3000';
+
+// DESCRIBE WHAT WE ARE TESTING
+  // SAY WHAT BEHAVIOR 'IT' AUGHT TO HAVE
+    // SEND THE REQUEST
+      // USE CHAI-EXPECT TO EXPECT THE STATUS RESULT
+      // CHECK FALSE VALUE TO SEE IF WE CAN MAKE TEST FAIL
+      // CALL DONE();
+
+//Test 1 - GET
+describe('Idea Hunt', function() {
+  it('should have a HTTP of 200 - success', function(done) {
+    request('http://localhost:3000', function(err, res, body) {
+      //Positive case
+      expect(res.statusCode).to.equal(200)
+      //Negative case
+      //expect(res.statusCode).to.equal(300)
+      done();
+    })
+  })
+});
+
+describe('Idea page', function() {
+  it('should have a HTTP of 200 - success', function(done) {
+    request('http://localhost:3000/ideas/55b151ab5032814c6df00017', function(err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      // expect(res.statusCode).to.equal(300)
+      done();
+    });
+  });
+});
+
+describe('Idea Hunt', function() {
+  it('The title header should include "The Product Hunt for Ideas"', function(done) {
+    request(baseUrl, function(err, res, body) {
+      var $ = cheerio.load(body);
+      var title = $('title').text();
+      expect(title).to.equal("The Product Hunt for Ideas");
+      // expect(title).to.equal('Moogle');
+      done();
+    });
+  });
+});
