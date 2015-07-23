f0b3cf5815d26f300b70e87ff90c2d6333200569
Manu Lohiya <manulohiya@gmail.com>
2015-07-23 09:27:46 -0700

    heroku deploy with many to many

--- models.js
+++ models.js
@@ -1,12 +1,86 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt'),
+    salt = bcrypt.genSaltSync(10);
 
-var IdeaSchema = new Schema({ company: String,
+var commentSchema = new Schema({
+  author: String,
+  desc: String
+  // idea: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: Idea
+  // },
+
+});
+
+var Comment = mongoose.model('Comment', commentSchema)
+
+var ideaSchema = new Schema({ 
+    company: String,
   	market: String,
-  	desc: String
+  	desc: String,
+    comments: [{
+      type: Schema.Types.ObjectId,
+      ref: "Comment" 
+    }] 
    });
 
+var Idea = mongoose.model('Idea', ideaSchema);
 
-var Idea = mongoose.model('Idea', IdeaSchema)
+var userSchema = new Schema({
+	email: String,
+	passwordDigest: String
+
+});
+
+
+
+// create a new user with secure (hashed) password
+userSchema.statics.createSecure = function (email, password, callback) {
+  // `this` references our schema
+  // store it in variable `that` because `this` changes context in nested callbacks
+  var that = this;
+
+  // hash password user enters at sign up
+  bcrypt.genSalt(function (err, salt) {
+    bcrypt.hash(password, salt, function (err, hash) {
+      console.log(hash);
+
+      // create the new user (save to db) with hashed password
+      that.create({
+        email: email,
+        passwordDigest: hash
+      }, callback);
+    });
+  });
+};
+
+// authenticate user (when user logs in)
+userSchema.statics.authenticate = function (email, password, callback) {
+  // find user by email entered at log in
+  this.findOne({email: email}, function (err, user) {
+    console.log(user);
+
+    // throw error if can't find user
+    if (user === null) {
+      throw new Error('Can\'t find user with email ' + email);
+
+    // if found user, check if password is correct
+    } else if (user.checkPassword(password)) {
+      callback(null, user);
+    }
+  });
+};
+
+// compare password user enters with hashed password (`passwordDigest`)
+userSchema.methods.checkPassword = function (password) {
+  // return hashing algorithm (with salt) on password user enters in order to compare with `passwordDigest`
+  return bcrypt.compareSync(password, this.passwordDigest);
+};
+
+
+var User = mongoose.model('User', userSchema);
 console.log("models.js: "+Idea);
-module.exports = Idea;
+module.exports.User = User;	
+module.exports.Idea = Idea;
+module.exports.Comment = Comment;
--- server.js
+++ server.js
@@ -2,13 +2,20 @@ var express = require('express'),
     app = express(),
     _ = require('underscore'),
     cors = require('cors'),
-    bodyParser = require('body-parser');
-    mongoose = require('mongoose');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    salt = bcrypt.genSaltSync(10),
+    session = require('express-session'),
+    db = require('./models');
 
 // Connect to database
 mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/startup');
-var Idea = require('./models');
-console.log(Idea);
+
+var baseUrl = "http://localhost:3000/"
+// var Idea = require('./models');
+// var User = require('./models');
+//console.log(Idea);
 
 // OPEN THE API TO REQUESTS FROM ANY DOMAIN
 app.use(cors());
@@ -18,17 +25,61 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname+"/public"));
 
 
+// middleware (new addition)
+// set session options
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: 'SuperSecretCookie',
+  cookie: { maxAge: 60000 }
+}));
+
+// middleware to manage sessions
+app.use('/', function (req, res, next) {
+  // saves userId in session for logged-in user
+  req.login = function (user) {
+    req.session.userId = user.id;
+  };
+
+  // finds user currently logged in based on `session.userId`
+  req.currentUser = function (callback) {
+    db.User.findOne({_id: req.session.userId}, function (err, user) {
+      req.user = user;
+      callback(null, user);
+    });
+  };
+
+ 
+
+
 app.get('/', function(req, res) {
   var index = __dirname + '/public/views/index.html';
   res.sendFile(index);
+});
+
+
+
+
+
+
+
+//Call for sessionID
+app.get('/api/users', function(req, res) {
+  req.currentUser(function (err, user) {
+    if (user) {
+      res.send(user);
+    } else {
+      res.send(null);
+    }
+  });
 });
 
 // Ideas Query
 app.get('/api/ideas', function (req, res) {
   // send all ideas as JSON response
   
-  console.log(Idea);
-  Idea.find(function(err, ideas){
+  //console.log(Idea);
+  db.Idea.find(function(err, ideas){
   console.log("Ideas: "+ideas);
   res.json(ideas);
 	});
@@ -37,16 +88,167 @@ app.get('/api/ideas', function (req, res) {
 // IDEAS#CREATE
 app.post('/api/ideas', function(req, res) {
   // SAVE LINE TO DB
-  var idea = new Idea({
+  var idea = new db.Idea({
     company: req.body.company,
     market: req.body.market,
     desc: req.body.desc
   });
-
-  idea.save(function(err, idea) {
+  console.log(idea);
+  idea.save(function(err,idea){ 
     res.json(idea);
   });
 });
+
+
+
+
+
+// // FEEDBACK#CREATE
+// app.post('/api/ideas/comment', function(req, res) {
+//   // SAVE LINE TO DB
+//   console.log("I am posting!");
+//   var comment = new db.Comment({
+//     author: req.body.author,    
+//     desc: req.body.desc
+//   });
+//   console.log(comment)
+//   comment.save(function(err, comment) {
+//     res.json(comment);
+//   });
+// });
+
+// Render comment page
+app.get('/ideas/:id', function(req, res) {
+  var comment = __dirname + '/public/views/idea.html';
+  res.sendFile(comment);
+});
+
+
+// Comment Query
+app.get('/api/ideas/comment', function (req, res) {
+  // send all ideas as JSON response
+  
+  //console.log(Idea);
+  db.Comment.find(function(err, comment){
+    console.log("Comment: "+comment);
+    res.json(comment);
+  });
+});
+
+// get all comment for one idea
+app.get('/api/ideas/:id', function(req, res){
+  // query the database to find the post indicated by the id
+  var targetId = req.params.id;
+  console.log("targetId: "+targetId);
+  console.log("Getting comments")
+  db.Idea
+    .find({_id: targetId})
+    .populate('comments')
+    .exec(function(err, idea) {
+      // RETURN IDEA
+      console.log("Executing");
+      console.log(idea);
+      res.json(idea);
+    });
+});
+
+// add a new comment to a idea
+app.post('/api/ideas/:id/comments', function(req, res){
+
+  console.log("Posting")
+  //var x = '55b007be66a6e8f3c957c53d';
+  var targetId = req.params.id
+  console.log("Target Id:" +targetId);
+  // create a new comment record
+  console.log(req.body.author);
+    var newComment = new db.Comment({
+      author:req.body.author,
+      desc:req.body.desc
+    });
+    newComment.save();
+    
+   // console.log("GUYS THE ID is" + x);
+  // query the database to find the post indicated by the id
+  db.Idea.findOne({_id:targetId}, function(err, idea){
+    var foundIdea = idea;
+    console.log('foundIdea: ' + foundIdea);
+    console.log("new comment:" +newComment);
+    console.log(newComment._id);
+    foundIdea.comments.push(newComment._id);
+    foundIdea.save(function (err, savedUser) {
+      res.json(savedUser);
+    });
+    //save the new comment
+    //   db.Idea.save(function(err, comment){
+    //   console.log("line 169, got here");
+    //   idea.comments.push(comment);
+    //   console.log("got past line 170");
+    //   console.log(idea);
+    //  // db.Idea.save(function(err,comment){;
+    //   // send the new comment as the JSON response
+    //   // res.json(comment);
+    //   // });
+    // });
+  });
+});
+
+
+
+
+// user submits the signup form
+app.post('/api/signup', function (req, res) {
+  console.log("Posting!");
+  // grab user data from params (req.body)
+  var email = req.body.email;
+  var password = req.body.password;
+
+  //console.log(newUser);
+  // create new user with secure password
+  db.User.createSecure(email, password, function (err, user) {
+    //res.send(user);
+    req.login(user);
+    res.redirect('/');
+  });
+});
+
+
+
+// user submits the login form
+app.post('/api/login', function (req, res) {
+  console.log("Logging in");
+  // grab user data from params (req.body)
+  var email = req.body.email;
+  var password = req.body.password;
+//  var userData = req.body.user;
+
+  // call authenticate function to check if password user entered is correct
+  db.User.authenticate(email, password, function (err, user) {
+    req.login(user);
+ //   res.send(user);
+   res.redirect('/');
+  });
+});
+
+
+// destroy `session.userId` to log out user
+  req.logout = function () {
+    console.log("Logging out");
+    req.session.userId = null;
+    req.user = null;
+
+  };
+
+    next();
+});
+
+// log out user (destroy session)
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+  
+});
+
+
 
 
 // set server to localhost:3000
